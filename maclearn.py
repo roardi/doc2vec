0f5850d7b5c662517cf18a136b1e109103e706be
Rolian Ardi <rolianardi@gmail.com>
2016-10-26 21:25:34 +0700

    Add files via upload

--- maclearn.py
+++ maclearn.py
@@ -1,7 +1,16 @@
+import MySQLdb
+import nltk
+import string
+import logging
+import sys,json
+import os
+import gensim
+
 # gensim modules
 from gensim import utils
 from gensim.models.doc2vec import TaggedDocument
 from gensim.models import Doc2Vec
+##from gensim.summarization import keywords
 
 # random shuffle
 from random import shuffle
@@ -10,12 +19,10 @@ from random import shuffle
 import numpy
 
 # classifier
-from sklearn.linear_model import LogisticRegression
+# from sklearn.linear_model import LogisticRegression
+from nltk.corpus import stopwords
 
-import logging
-import sys,json
-
-log = logging.getLogger()
+##log = logging.getLogger()
 ##log.setLevel(logging.DEBUG)
 
 ch = logging.StreamHandler(sys.stdout)
@@ -55,25 +62,9 @@ class TaggedLineSentence(object):
         shuffle(self.sentences)
         return self.sentences
 
-
-from gensim.summarization import keywords
-import MySQLdb
-import nltk
-import string
-##import six
-##import rake
-##import operator
-##import io
-
 # connect
 db = MySQLdb.connect(host="localhost", user="root", passwd="", db="buds")
 cursor = db.cursor()
-
-### EXAMPLE ONE - SIMPLE
-##stoppath = "stopwords.txt"
-##
-### 1. initialize RAKE by providing a path to a stopwords file
-##rake_object = rake.Rake(stoppath, 4, 1, 1)
 
 # pass variable from PHP
 ##try:
@@ -83,14 +74,16 @@ cursor = db.cursor()
 ##    print "ERROR"
 ##    sys.exit(1)
 grouppost_id = '12'
-##datas = [str(x) for x in data]
-from nltk.corpus import stopwords
+
 raw = open("C:\Python27\word2vec\stopwords.txt", "rU")
 stop = raw.read()
 
-cursor.execute("SELECT posts FROM groupposts WHERE grouppost_id=%s",grouppost_id)
-test = cursor.fetchone()
-strtest = str(test).lower()
+cursor.execute("SELECT grouppost_id,posts FROM groupposts WHERE grouppost_id=%s",grouppost_id)
+test_fetch = cursor.fetchall()
+for tes in test_fetch:
+    gpid = tes[0]
+    post = tes[1]
+strtest = str(post).lower()
 test_punc = "".join(l for l in strtest if l not in string.punctuation)
 test_tok = nltk.word_tokenize(test_punc)
 test_stop = [i for i in test_tok if i not in stop]
@@ -99,18 +92,10 @@ new_test = " ".join(test_stop)
 with open("Test.txt", "w") as text_file:
     text_file.writelines(new_test)
  
-print 'Uji:'
-print test
 ##uji = keywords(new_test,split=True)
-##print 'Keyword uji'
-##print uji
-
-### 2. run on RAKE on a given text
-##uji2 = rake_object.run(datauji)
-##print str(uji2).split()
 
 # execute SQL select statement
-cursor.execute("SELECT posts FROM groupposts WHERE group_id='1' ")
+cursor.execute("SELECT posts FROM groupposts WHERE group_id='1' AND status='2' ")
 
 # commit your changes
 db.commit()
@@ -121,8 +106,10 @@ numrows = int(cursor.rowcount)
 # create sources
 with open("Output.txt", "w") as text_file:
     for x in range(0,numrows):
-        data = cursor.fetchone()
-        strdata = str(data).lower()
+        data_fetch = cursor.fetchone()
+        for datas in data_fetch:
+            data1 = datas
+        strdata = str(data1).lower()
         data_punc = "".join(l for l in strdata if l not in string.punctuation)
         data_tok = nltk.word_tokenize(data_punc)
         data_stop = [i for i in data_tok if i not in stop]
@@ -130,69 +117,82 @@ with open("Output.txt", "w") as text_file:
         with open("Output.txt", "a") as text_file:
             text_file.writelines(new_data)
             text_file.writelines("\n")
-
-sources = {'Output.txt':'TRAIN_POS','Test.txt':'TEST'}
+            
+train_sources = {'Output.txt':'TRAIN','Test.txt':'TEST'}
+##test_sources = {'Test.txt':'TEST'}
 
 ##log.info('TaggedDocument')
-sentences = TaggedLineSentence(sources)
+sentences = TaggedLineSentence(train_sources)
+##test_sentences = TaggedLineSentence(test_sources)
 
 ##log.info('D2V')
-model = Doc2Vec(min_count=1, window=5, size=100, sample=1e-4, negative=5, workers=8, alpha=0.025, hs=1, sorted_vocab=1, iter=5)
-model.build_vocab(sentences.to_array())
+model = Doc2Vec(min_count=1, window=5, size=100, workers=3, alpha=0.025, sorted_vocab=1, iter=5, batch_words=5000)
+model.build_vocab(sentences)
 
 ##log.info('Epoch')
-for epoch in range(3):
-	log.info('EPOCH: {}'.format(epoch))
-	model.train(sentences.sentences_perm())
+##for epoch in range(5):
+##    log.info('EPOCH: {}'.format(epoch))
+model.train(sentences)
 
 ##log.info('Model Save')
-model.save('./imdb.d2v')
-model = Doc2Vec.load('./imdb.d2v')
+model.save('./maclearn.d2v')
+model = Doc2Vec.load('./maclearn.d2v')
+
+##print model.vocab
+
+def json_list(array1,array2,array3):
+    arr = []
+    for i in range(0,len(array1)):
+        d = {}
+        arr.append({"id":array1[i],"data":array2[i],"similarity":array3[i]})
+    return arr
 
 #loop for similarity
-cursor.execute("SELECT posts FROM groupposts WHERE group_id='1'")
+cursor.execute("SELECT grouppost_id,posts FROM groupposts WHERE group_id='1' AND status='2'")
+row = cursor.fetchone()
 numrow = int(cursor.rowcount)
+
 max_sim = 0
-for i in range (0,numrow):
-    row = cursor.fetchone()
-    strrow = str(row).lower()
+id_row = []
+isi = []
+sims = []
+
+while row is not None:
+    gpid_row = row[0]
+    post_row = row[1]
+    
+    strrow = str(post_row).lower()
     row_punc = "".join(l for l in strrow if l not in string.punctuation)
     row_tok = nltk.word_tokenize(row_punc)
     row_stop = [i for i in row_tok if i not in stop]
     new_row = " ".join(row_stop)
-
+    
     with open("Data.txt", "w") as text_file:
         text_file.writelines(new_row)
 
     result = model.n_similarity(test_stop,row_stop)
-    if result>0.3:
+##    result = round(100*similar,2)
+    if result>=0.5:
+        id_row.append(gpid_row)
+        isi.append(post_row)        
+        sims.append(result)
         if max_sim < result:
             max_sim = result
         else:
             max_sim = max_sim
-        print 'Data:'
-        print row
-        ##data = keywords(new_row,split=True)
-        ##print 'Keyword data:'
-        ##print data
 
-        ### 2. run on RAKE on a given text
-        ##data2 = rake_object.run(text)
-        ##print str(data2).split()
-
-        ##print model.vocab
-        ##print model.most_similar('rumah')
-        print "Similarity: "
-        print model.n_similarity(test_stop,row_stop)
+    row = cursor.fetchone()
         
-print "Maximal: "
-print max_sim
+arrr = json_list(id_row,isi,sims)
+row = {
+    "gpid":gpid,
+    "uji":post,
+    "row":arrr,       
+    "max":max_sim
+    }
+print json.dumps(row)
 
-
-
-
-
-
+  
 
 
 
